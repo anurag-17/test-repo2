f6a59a2810a914a367e2e658ba2a80a5400b9ae0
Jay <jayantchoudhary271@gmail.com>
2023-11-30 18:39:41 +0530

    Backend Working

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 // mongodb+srv://test:test123@test.wbkraoh.mongodb.net/?retryWrites=true&w=majority
+console.log("Current working directory:", process.cwd());
 const connectDB = async () => {
   await mongoose.connect(process.env.URL, {
     useNewUrlParser: true,
