ae7d74a9f8250fe7209ac22bf758702f3892a98d
anurag.pandey17 <anurag.pandey17@gmail.com>
2023-11-30 17:48:52 +0530

    deploy in process

--- server/index.js
+++ server/index.js
@@ -263,10 +263,10 @@ app.get("/summaryData/:id", async (req, res) => {
 
 if (process.env.NODE_ENV === "dev") {
   //replaced "production" with "dev"
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
