55d7320d30bcfde2235e3e295166306f6914acd1
Jay <jayantchoudhary271@gmail.com>
2023-11-30 15:02:42 +0530

    code fix

--- server/index.js
+++ server/index.js
@@ -29,10 +29,10 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(cookieParser('secret'));
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "5mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 
 
