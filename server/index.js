0af6019595898659974ff65f64954348926d0bb8
anurag.pandey17 <anurag.pandey17@gmail.com>
2023-11-30 17:52:37 +0530

    m,bghjj

--- server/index.js
+++ server/index.js
@@ -266,7 +266,7 @@ if (process.env.NODE_ENV === "dev") {
   app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
